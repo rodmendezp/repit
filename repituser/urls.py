e1e916d059927186c6cf888613343e481ced34b6
Rodrigo Mendez <rodmendezp@gmail.com>
2018-07-30 23:39:02 -0400

    added sign up

--- repituser/urls.py
+++ repituser/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
-
+from django.conf.urls import url
 from . import views
 
+app_name = 'user'
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    url(r'^register/$', views.UserFormView.as_view(), name='register'),
 ]
--- repituser/views.py
+++ repituser/views.py
@@ -1,8 +1,11 @@
-import pytz
 import datetime
 
 from django.http import HttpResponse
 from django.utils import timezone
+from django.views import View
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .forms import UserForm
 
 
 def index(request):
@@ -11,3 +14,33 @@ def index(request):
     time_local = timezone.localtime()
     return HttpResponse("timezone.now() = " + str(time) + "\n datetime.now() = " + str(datetime_time) +
                         "\n time_local = " + str(time_local))
+
+
+class UserFormView(View):
+    form_class = UserForm
+    template_name = 'repituser/registration_form.html'
+
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            # Creates an object from the form, but does not save it to the database
+            user = form.save(commit=False)
+
+            # Normalize values and use hashing function to encrypt password
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+
+            # Returns User object if credentials are correct
+            user = authenticate(username=username, password=password)
+            if user is not None and user.is_active:
+                login(request, user)
+                return redirect('label:index')
+
+        # If form not valid or user is inactive then redirect to the same form
+        return render(request, self.template_name, {'form': form})
