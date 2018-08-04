56ba24114787070bf77a8751afe9ada077feb180
Rodrigo Mendez <rodmendezp@gmail.com>
2018-08-03 23:46:06 -0400

    added STATIC_ROOT and WEBPACK_LOADER

--- Repit/settings.py
+++ Repit/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'repituser.apps.RepituserConfig',
+    'frontend.apps.FrontendConfig',
+    'webpack_loader',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+# Webpack module loader. Set them inside each app.
+WEBPACK_LOADER = {}
