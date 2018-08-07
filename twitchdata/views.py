1f0b69a9b08084bcc660a30585ebdce10cf9c00e
Rodrigo Mendez <rodmendezp@gmail.com>
2018-08-07 13:31:12 -0400

    added basic view to twitchdata rest api

--- twitchdata/views.py
+++ twitchdata/views.py
@@ -1,3 +1,93 @@
-from django.shortcuts import render
+from twitchdata.serializers import *
+from twitchdata.models import *
+from rest_framework import generics
 
-# Create your views here.
+
+class TwitchUserList(generics.ListCreateAPIView):
+    queryset = TwitchUser.objects.all()
+    serializer_class = TwitchUserSerializer
+
+
+class TwitchUserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = TwitchUser.objects.all()
+    serializer_class = TwitchUserSerializer
+
+
+class ChannelList(generics.ListCreateAPIView):
+    queryset = Channel.objects.all()
+    serializer_class = ChannelSerializer
+
+
+class ChannelDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Channel.objects.all()
+    serializer_class = ChannelSerializer
+
+
+class StreamerList(generics.ListCreateAPIView):
+    queryset = Streamer.objects.all()
+    serializer_class = StreamerSerializer
+
+
+class StreamerDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Streamer.objects.all()
+    serializer_class = StreamerSerializer
+
+
+class GameList(generics.ListCreateAPIView):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
+
+
+class GameDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
+
+
+class VideoList(generics.ListCreateAPIView):
+    queryset = Video.objects.all()
+    serializer_class = VideoSerializer
+
+
+class VideoDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Video.objects.all()
+    serializer_class = VideoSerializer
+
+
+class ClipList(generics.ListCreateAPIView):
+    queryset = Clip.objects.all()
+    serializer_class = ClipSerializer
+
+
+class ClipDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Clip.objects.all()
+    serializer_class = ClipSerializer
+
+
+class EmoticonList(generics.ListCreateAPIView):
+    queryset = Emoticon.objects.all()
+    serializer_class = EmoticonSerializer
+
+
+class EmoticonDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Emoticon.objects.all()
+    serializer_class = EmoticonSerializer
+
+
+class ChatList(generics.ListCreateAPIView):
+    queryset = Chat.objects.all()
+    serializer_class = ChatSerializer
+
+
+class ChatDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Chat.objects.all()
+    serializer_class = ChatSerializer
+
+
+class MessageList(generics.ListCreateAPIView):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+
+
+class MessageDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
