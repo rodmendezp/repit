577a077aec47b2a68d3ea631b27e5cdf76df99c9
Rodrigo Mendez <rodmendezp@gmail.com>
2018-08-07 21:17:40 -0400

    added custom create and update for nested StreamerSerializer but probably will change soon to a more general form

--- twitchdata/serializers.py
+++ twitchdata/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.db import models
 from twitchdata.models import *
 
 
@@ -15,9 +16,43 @@ class ChannelSerializer(serializers.ModelSerializer):
 
 
 class StreamerSerializer(serializers.ModelSerializer):
+    twitch_user = TwitchUserSerializer()
+    channel = ChannelSerializer()
+
+    def create(self, validated_data):
+        twitch_user_data = validated_data.pop('twitch_user')
+        twitch_user = TwitchUser.objects.create(**twitch_user_data)
+        channel_data = validated_data.pop('channel')
+        channel = Channel.objects.create(**channel_data)
+        streamer = Streamer.objects.create(twitch_user=twitch_user, channel=channel)
+        return streamer
+
+    def update(self, instance, validated_data):
+        twitch_user_data = validated_data.get('twitch_user', None)
+        twitch_user_id = twitch_user_data.get('id', None)
+        if twitch_user_id:
+            try:
+                twitch_user = TwitchUser.objects.get(pk=twitch_user_id)
+            except models.ObjectDoesNotExist:
+                return
+        channel_data = validated_data.get('channel', None)
+        channel_id = channel_data.get('id', None)
+        if channel_id:
+            try:
+                channel = Channel.objects.get(pk=channel_id)
+            except models.ObjectDoesNotExist:
+                return
+        streamer = instance
+        if twitch_user:
+            streamer.twitch_user = twitch_user
+        if channel:
+            streamer.channel = channel
+        streamer.save()
+        return streamer
+
     class Meta:
         model = Streamer
-        fields = ()
+        fields = ('twitch_user', 'channel')
 
 
 class GameSerializer(serializers.ModelSerializer):
