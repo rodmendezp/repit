5a89293da3ccdca9b5359be9303fab625c4cdf00
Rodrigo Mendez <rodmendezp@gmail.com>
2018-08-14 15:57:56 -0300

    added query parameters to some models ListCreateAPIViews

--- twitchdata/views.py
+++ twitchdata/views.py
@@ -12,7 +12,15 @@ class TwitchDataLOPagination(LimitOffsetPagination):
 class TwitchUserList(generics.ListCreateAPIView):
     queryset = TwitchUser.objects.all()
     serializer_class = TwitchUserSerializer
-    # pagination_class = TwitchDataLOPagination
+
+    # TODO: See if it is viable to have a generic 'get_queryset' function
+    def get_queryset(self):
+        queryset = TwitchUser.objects.all()
+        twid = self.request.query_params.get('twid', None)
+        name = self.request.query_params.get('name', None)
+        queryset = queryset.filter(twid=twid) if twid else queryset
+        queryset = queryset.filter(name=name) if name else queryset
+        return queryset
 
 
 class TwitchUserDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -34,6 +42,12 @@ class StreamerList(generics.ListCreateAPIView):
     queryset = Streamer.objects.all()
     serializer_class = StreamerSerializer
 
+    def get_queryset(self):
+        queryset = Streamer.objects.all()
+        streamer_name = self.request.query_params.get('streamer_name', None)
+        queryset = queryset.filter(twitch_user__name=streamer_name) if streamer_name else queryset
+        return queryset
+
 
 class StreamerDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Streamer.objects.all()
@@ -43,6 +57,12 @@ class StreamerDetail(generics.RetrieveUpdateDestroyAPIView):
 class GameList(generics.ListCreateAPIView):
     queryset = Game.objects.all()
     serializer_class = GameSerializer
+
+    def get_queryset(self):
+        queryset = Game.objects.all()
+        name = self.request.query_params.get('name', None)
+        queryset = queryset.filter(name=name) if name else queryset
+        return queryset
 
 
 class GameDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -54,6 +74,12 @@ class VideoList(generics.ListCreateAPIView):
     queryset = Video.objects.all()
     serializer_class = VideoSerializer
 
+    def get_queryset(self):
+        queryset = Video.objects.all()
+        twid = self.request.query_params.get('twid', None)
+        queryset = queryset.filter(twid=twid) if twid else queryset
+        return queryset
+
 
 class VideoDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Video.objects.all()
@@ -63,6 +89,16 @@ class VideoDetail(generics.RetrieveUpdateDestroyAPIView):
 class ClipList(generics.ListCreateAPIView):
     queryset = Clip.objects.all()
     serializer_class = ClipSerializer
+
+    def get_queryset(self):
+        queryset = Clip.objects.all()
+        streamer_name = self.request.query_params.get('streamer_name', None)
+        video_twid = self.request.query_params.get('video_twid', None)
+        slug = self.request.query_params.get('slug', None)
+        queryset = queryset.filter(streamer__twitch_user__name=streamer_name) if streamer_name else queryset
+        queryset = queryset.filter(video__twid=video_twid) if video_twid else queryset
+        queryset = queryset.filter(slug=slug) if slug else queryset
+        return queryset
 
 
 class ClipDetail(generics.RetrieveUpdateDestroyAPIView):
