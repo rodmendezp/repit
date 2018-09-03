59cb02156556fccea0bd7bdd99978c777064115f
Rodrigo Mendez <rodmendezp@gmail.com>
2018-09-03 12:46:17 -0300

    added bootstrap to landing

--- frontend/src/landing.js
+++ frontend/src/landing.js
@@ -1,8 +1,12 @@
 /* eslint-disable no-new */
 import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 import Landing from './LandingApp';
 
 Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
 
 new Vue({
     el: '#landing',
