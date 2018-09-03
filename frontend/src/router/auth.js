8c13ad7764c559f87c05c5ffa499cb74296c8021
Rodrigo Mendez <rodmendezp@gmail.com>
2018-09-03 12:47:41 -0300

    added singup to router

--- frontend/src/router/auth.js
+++ frontend/src/router/auth.js
@@ -1,15 +1,19 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import SignInView from '@/components/auth/SignInView';
+import SignUpView from '@/components/auth/SignUpView';
 
 Vue.use(Router);
-
 
 export default new Router({
     routes: [
         {
             path: '/',
             component: SignInView,
+        },
+        {
+            path: '/signup',
+            component: SignUpView,
         },
     ],
 });
