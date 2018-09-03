bf56cc06bce14c1ff52ff52c4a6b87795ba50164
Rodrigo Mendez <rodmendezp@gmail.com>
2018-09-03 12:48:43 -0300

    change sign in view

--- frontend/src/components/auth/SignInView.vue
+++ frontend/src/components/auth/SignInView.vue
@@ -1,17 +1,21 @@
 <template>
-    <div>
-        <h1>Welcome!</h1>
-        <form ref="form" method="post" @submit.prevent="onSubmit">
-            <label for="id_email">Email</label>
-            <input v-model="form.email.value"
-                   placeholder="your@email.com" required
-                   ref="email" type="email" name="email" id="id_email"/>
-            <label for="id_password">Password</label>
-            <input v-model="form.password.value"
-                   ref="password" type="password" name="password" id="id_password"
-                   placeholder="Password" required/>
-        </form>
-        <p>Do not have an account? <router-link to="signup" tag="button">Sign up</router-link></p>
+    <div class="row full-height align-items-center">
+        <div class="col"></div>
+        <div class="col">
+            <h1>Welcome!</h1>
+            <form ref="form" method="post" @submit.prevent="onSubmit">
+                <label for="id_email">Email</label>
+                <input v-model="form.email.value"
+                       placeholder="your@email.com" required
+                       ref="email" type="email" name="email" id="id_email"/>
+                <label for="id_password">Password</label>
+                <input v-model="form.password.value"
+                       ref="password" type="password" name="password" id="id_password"
+                       placeholder="Password" required/>
+            </form>
+            <p>Do not have an account? <router-link to="signup" tag="button">Sign up</router-link></p>
+        </div>
+        <div class="col"></div>
     </div>
 </template>
 
@@ -33,8 +37,6 @@
     };
 </script>
 
-<style scoped>
-    h1, h2 {
-      font-weight: normal;
-    }
+<style lang="sass">
+    @import "~@/styles/auth/main.scss"
 </style>
