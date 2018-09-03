4827c60442e40932bad914713482ee9d19f3f579
Rodrigo Mendez <rodmendezp@gmail.com>
2018-09-03 12:38:07 -0300

    reformatted webpack conf file

--- frontend/build/webpack.base.conf.js
+++ frontend/build/webpack.base.conf.js
@@ -56,7 +56,12 @@ module.exports = {
             {
                 test: /\.js$/,
                 loader: 'babel-loader',
-                include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
+                include: [
+                    resolve('src'),
+                    resolve('test'),
+                    resolve('bootstrap-vue'),
+                    resolve('node_modules/webpack-dev-server/client')
+                ]
             },
             {
                 test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
