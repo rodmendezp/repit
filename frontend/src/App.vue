4b08bc85e3be42fddfe31b58af7440f171f9a3bf
Rodrigo Mendez <rodmendezp@gmail.com>
2018-09-05 13:39:44 -0300

    save user info on Store

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,36 @@
 <template>
-    <div id="app">
+    <div id="app" class="container-fluid full-height" v-cloak>
         <router-view/>
     </div>
 </template>
 
 <script>
+    import { mapMutations } from 'vuex';
+
     export default {
         name: 'App',
+        methods: {
+            ...mapMutations({
+                setUser: 'setUser',
+            }),
+        },
+        mounted() {
+            /* eslint no-undef: 0 */
+            const userSplit = LOGGED_USER.value.split(';');
+            if (userSplit.length === 3) {
+                this.setUser({
+                    first_name: userSplit[0],
+                    last_name: userSplit[1],
+                    email: userSplit[2],
+                });
+            } else {
+                console.log('Somehting went wrong, maybe user not logged?');
+                console.log('LOGGED_USER.value = ', LOGGED_USER.value);
+            }
+        },
     };
 </script>
 
 <style lang="sass">
-    #app
-        padding: 0
-        margin: 0
+    @import "~@/styles/app/main.scss"
 </style>
