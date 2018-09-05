90e10d7b52ebeb9122deca5a7fa9ccef129f0998
Rodrigo Mendez <rodmendezp@gmail.com>
2018-09-04 21:54:42 -0300

    added EmailLogin view

--- frontend/urls.py
+++ frontend/urls.py
@@ -6,6 +6,7 @@ from backend.requests import is_user
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^accounts/$', views.auth),
+    url(r'^accounts/login/$', views.EmailLoginView.as_view()),
     url(r'^accounts/register/', views.UserCreate.as_view()),
     url(r'^accounts/is_user/', is_user),
     url(r'^app/$', views.app),
