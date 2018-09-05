c6ecac1dbf99eb6dfd85360da6832188dbd9a76e
Rodrigo Mendez <rodmendezp@gmail.com>
2018-09-05 13:41:24 -0300

    get user and print it

--- frontend/src/components/HomeView.vue
+++ frontend/src/components/HomeView.vue
@@ -1,25 +1,34 @@
 <template>
-    <div>
-        <b-button @click="startLabeling">Start Labeling</b-button>
+    <div class="row full-height align-items-center">
+        <div class="col"></div>
+        <div style="text-align: center" class="col">
+            <h1 v-if="user"> Welcome {{ user.first_name }}</h1>
+            <button class="btn btn-primary" @click="startLabeling">Start Labeling</button>
+        </div>
+        <div class="col"></div>
     </div>
 </template>
 
 <script>
-    import bButton from 'bootstrap-vue/es/components/button/button';
+    import { mapGetters } from 'vuex';
 
     export default {
         name: 'HomeView',
-        components: {
-            bButton,
-        },
         methods: {
             startLabeling() {
                 this.$router.push('/label/292622008/311/330');
             },
         },
+        computed: {
+            ...mapGetters({
+                user: 'getUser',
+            }),
+        },
+        mounted() {
+        },
     };
 </script>
 
-<style scoped>
+<style>
 
 </style>
