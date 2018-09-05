2d01c007bcf3acd5ce7c74de530e9e1988084408
Rodrigo Mendez <rodmendezp@gmail.com>
2018-09-04 21:56:04 -0300

    removed repituser app

--- repituser/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RepituserConfig(AppConfig):
-    name = 'repituser'
--- repituser/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-app_name = 'user'
-
-urlpatterns = [
-    url(r'^register/$', views.UserFormView.as_view(), name='register'),
-]
