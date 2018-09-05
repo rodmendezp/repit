0d2f72484a90ebedc3640f4a2e1ff62bde495a10
Rodrigo Mendez <rodmendezp@gmail.com>
2018-09-04 21:51:06 -0300

    added backend to subdomain but need to read more about it

--- backend/apps.py
+++ backend/apps.py
@@ -1,5 +1,15 @@
 from django.apps import AppConfig
+from django.conf import settings
 
 
 class BackendConfig(AppConfig):
     name = 'backend'
+    label = 'backend'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._set_subdomain()
+
+    def _set_subdomain(self):
+        # settings.SUBDOMAIN_URLCONFS['kabara'] = self.name + '.urls'
+        pass
