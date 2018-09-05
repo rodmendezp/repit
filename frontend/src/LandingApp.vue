ec11aca1d0f5719ded5da06cd013d26115e17202
Rodrigo Mendez <rodmendezp@gmail.com>
2018-09-05 13:41:05 -0300

    change location.href to app and if not logged it will redirect to account

--- frontend/src/LandingApp.vue
+++ frontend/src/LandingApp.vue
@@ -24,7 +24,7 @@
         },
         methods: {
             signIn() {
-                location.href = 'http://127.0.0.1:8000/accounts';
+                location.href = 'http://127.0.0.1:8000/app/';
             },
         },
     };
