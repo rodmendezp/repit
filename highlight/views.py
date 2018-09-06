b1110c8c0d55d29d0645ea6492d7ba0e37eb0121
Rodrigo Mendez <rodmendezp@gmail.com>
2018-09-06 16:01:23 -0300

    added views for type and creating custom put for highlight

--- highlight/views.py
+++ highlight/views.py
@@ -1,6 +1,18 @@
-from .serializers import HighlightSerializer
-from .models import Highlight
-from rest_framework import generics
+from .serializers import HighlightSerializer, TypeSerializer
+from .models import Highlight, Type
+from django.http import Http404
+from rest_framework import generics, status
+from rest_framework.response import Response
+
+
+class TypeList(generics.ListCreateAPIView):
+    queryset = Type.objects.all()
+    serializer_class = TypeSerializer
+
+
+class TypeDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Type.objects.all()
+    serializer_class = TypeSerializer
 
 
 class HighlightList(generics.ListCreateAPIView):
@@ -11,3 +23,18 @@ class HighlightList(generics.ListCreateAPIView):
 class HighlightDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Highlight.objects.all()
     serializer_class = HighlightSerializer
+
+    @staticmethod
+    def get_by_pk(pk):
+        try:
+            return Highlight.objects.get(pk=pk)
+        except Highlight.DoesNotExist:
+            raise Http404
+
+    def put(self, request, *args, **kwargs):
+        highlight = self.get_by_pk(kwargs['pk'])
+        serializer = HighlightSerializer(highlight, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
