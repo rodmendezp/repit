d5dded98a1a0a19e42cc5e1248983178b2b3507c
Rodrigo Mendez <rodmendezp@gmail.com>
2018-09-06 15:59:14 -0300

    changed type of times to string to get rid of warning and comment console logs for events

--- frontend/src/components/player/TwitchPlayer.vue
+++ frontend/src/components/player/TwitchPlayer.vue
@@ -10,8 +10,8 @@
         name: 'TwitchPlayer',
         props: {
             videoId: { type: String },
-            stTime: { type: Number },
-            endTime: { type: Number },
+            stTime: { type: String },
+            endTime: { type: String },
         },
         components: {},
         data() {
@@ -42,21 +42,21 @@
         mounted() {
             this.options.video = this.videoId;
             this.loadScript('http://player.twitch.tv/js/embed/v1.js', 'Twitch').then((Twitch) => {
-                console.log('Twitch Script Loaded');
+                // console.log('Twitch Script Loaded');
                 this.Twitch = Twitch;
                 this.player = new Twitch.Player(`Twitch-Player-${this.uid}`, this.options);
                 this.player.setVolume(0.5);
                 this.player.addEventListener(Twitch.Player.READY, () => {
-                    console.log('Twitch Player READY');
+                    // console.log('Twitch Player READY');
                 });
                 this.player.addEventListener(Twitch.Player.PLAY, () => {
-                    console.log('Twitch Player PLAY');
+                    // console.log('Twitch Player PLAY');
                 });
                 this.player.addEventListener(Twitch.Player.PAUSE, () => {
-                    console.log('Twitch Player PAUSE');
+                    // console.log('Twitch Player PAUSE');
                 });
                 this.player.addEventListener(Twitch.Player.PLAYING, () => {
-                    console.log('Twitch Player PLAYING');
+                    // console.log('Twitch Player PLAYING');
                     if (this.seekAtInstantPlay) {
                         this.player.seek(this.stTime);
                         this.seekAtInstantPlay = false;
