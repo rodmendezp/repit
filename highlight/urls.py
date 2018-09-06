397fa2527eb679ce97047db1940275a2a55af582
Rodrigo Mendez <rodmendezp@gmail.com>
2018-09-06 16:00:58 -0300

    added type views to urls

--- highlight/urls.py
+++ highlight/urls.py
@@ -3,6 +3,8 @@ from highlight import views
 
 
 urlpatterns = [
+    url(r'^type/$', views.TypeList.as_view()),
+    url(r'^type/(?P<pk>[0-9]+)$', views.TypeDetail.as_view()),
     url(r'^highlight/$', views.HighlightList.as_view()),
     url(r'^highlight/(?P<pk>[0-9]+)$', views.HighlightDetail.as_view()),
 ]
