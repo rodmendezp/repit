3402ccd410711bdf6ab4fffdd4889964dffaf220
Rodrigo Mendez <rodmendezp@gmail.com>
2018-09-13 03:43:58 -0300

    added Access-Control-Allow-Origin to load file (in this case the problem was with font-awesome file) from webserver in development

---
+++ frontend/build/dev-client.js
@@ -0,0 +1,11 @@
+// /* eslint-disable */
+// 'use strict'
+//
+// // Doc: https://www.npmjs.com/package/webpack-hot-middleware
+// var hotClient = require('webpack-hot-middleware/client?noInfo=true')
+//
+// hotClient.subscribe(function (event) {
+//     if (event.action === 'reload') {
+//         window.location.reload()
+//     }
+// })
