3cf282f33f358d3ed10f7165f05570b6c2564831
Rodrigo Mendez <rodmendezp@gmail.com>
2018-09-27 22:59:07 -0300

    moved http request functions to here and add websocket

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,21 +2,64 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import highlight from './modules/highlight';
 import player from './modules/player';
+import filler from './modules/filler';
 
 Vue.use(Vuex);
 
+function JSONHttpRequest(resolve, reject, method, params) {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, params.baseURL, true);
+    xhr.setRequestHeader('X-CSRFToken', params.csrfToken);
+    xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+            let response;
+            if (xhr.getResponseHeader('content-type') === 'application/json') {
+                response = JSON.parse(xhr.response);
+            } else response = xhr.response;
+            if (response.status === 'ERROR') reject();
+            else resolve(response);
+        }
+    };
+    xhr.onerror = () => { reject(); };
+    xhr.send(JSON.stringify(params.data));
+}
+
+function GetHttpRequest(resolve, reject, url) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+            if (xhr.status !== 200) {
+                reject();
+                return;
+            }
+            const response = JSON.parse(xhr.response);
+            if (response.status === 'ERROR') reject();
+            else resolve(response);
+        }
+    };
+    xhr.onerror = () => { reject(); };
+    xhr.send();
+}
+
 const state = {
     user: null,
+    webSocket: null,
 };
 
 const mutations = {
     setUser(s, user) {
         s.user = user;
     },
+    setWebSocket(s, webSocket) {
+        s.webSocket = webSocket;
+    },
 };
 
 const getters = {
     getUser: s => s.user,
+    getWebSocket: s => s.webSocket,
 };
 
 const actions = {};
@@ -29,6 +72,7 @@ const store = new Vuex.Store({
     modules: {
         highlight,
         player,
+        filler,
     },
 });
 
@@ -40,7 +84,9 @@ const storeConfig = {
     modules: {
         highlight,
         player,
+        filler,
     },
 };
 
 export { store as default, storeConfig };
+export { JSONHttpRequest, GetHttpRequest };
