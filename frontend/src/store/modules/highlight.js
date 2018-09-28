22818df8bc08f212daa47bf271279ed89a02d139
Rodrigo Mendez <rodmendezp@gmail.com>
2018-09-27 22:57:32 -0300

    add status and highlight to store and move Http Requests to index.js

--- frontend/src/store/modules/highlight.js
+++ frontend/src/store/modules/highlight.js
@@ -1,41 +1,5 @@
 // import Vue from 'vue';
-
-function JSONHttpRequest(resolve, reject, method, params) {
-    const xhr = new XMLHttpRequest();
-    xhr.open(method, params.baseURL, true);
-    xhr.setRequestHeader('X-CSRFToken', params.csrfToken);
-    xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState === 4) {
-            let response;
-            if (xhr.getResponseHeader('content-type') === 'application/json') {
-                response = JSON.parse(xhr.response);
-            } else response = xhr.response;
-            if (response.status === 'ERROR') reject();
-            else resolve(response);
-        }
-    };
-    xhr.onerror = () => { reject(); };
-    xhr.send(JSON.stringify(params.data));
-}
-
-function GetHttpRequest(resolve, reject, url) {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState === 4) {
-            if (xhr.status !== 200) {
-                reject();
-                return;
-            }
-            const response = JSON.parse(xhr.response);
-            if (response.status === 'ERROR') reject();
-            else resolve(response);
-        }
-    };
-    xhr.onerror = () => { reject(); };
-    xhr.send();
-}
+import { JSONHttpRequest, GetHttpRequest } from '../index';
 
 const config = {
     baseURL: '/highlight/',
@@ -43,15 +7,25 @@ const config = {
 
 const localState = {
     types: null,
+    highlight: null,
+    status: 'idle',
 };
 
 const getters = {
     getTypes: s => s.types,
+    getHighlight: s => s.highlight,
+    getStatus: s => s.status,
 };
 
 const mutations = {
     setTypes(s, types) {
         s.types = types;
+    },
+    setHighlight(s, highlight) {
+        s.highlight = highlight;
+    },
+    setStatus(s, status) {
+        s.status = status;
     },
 };
 
