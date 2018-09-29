137f16d9f9f762b5b005f6f06c3a2824b13bdaf9
Rodrigo Mendez <rodmendezp@gmail.com>
2018-09-29 13:41:44 -0300

    modified post of video

--- highlight/views.py
+++ highlight/views.py
@@ -1,9 +1,23 @@
 from .serializers import HighlightSerializer, TypeSerializer
 from .models import Highlight, Type
+from twitchdata.models import Video
 from django.http import Http404
 from datetime import datetime
 from rest_framework import generics, status
 from rest_framework.response import Response
+from datetime import time
+
+
+def seconds_to_hms(seconds):
+    h = int(seconds / 3600)
+    seconds -= h * 3600
+    m = int(seconds / 60)
+    seconds -= m * 60
+    return {
+        'hour': h,
+        'minute': m,
+        'second': seconds
+    }
 
 
 class TypeList(generics.ListCreateAPIView):
@@ -19,6 +33,34 @@ class TypeDetail(generics.RetrieveUpdateDestroyAPIView):
 class HighlightList(generics.ListCreateAPIView):
     queryset = Highlight.objects.all()
     serializer_class = HighlightSerializer
+
+    # TODO: VideoID should be changed for VideoTWID to not confuse
+    def post(self, request, *args, **kwargs):
+        required_fields = ['video', 'start', 'end']
+        for field in required_fields:
+            if not request.data.get(field):
+                return Response(data=None, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            video = Video.objects.get(twid=request.data['video'])
+        except Video.DoesNotExist as e:
+            return Response(data=repr(e), status=status.HTTP_400_BAD_REQUEST)
+        try:
+            type = Type.objects.get(name=request.data['type'])
+        except Type.DoesNotExist as e:
+            return Response(data=repr(e), status=status.HTTP_400_BAD_REQUEST)
+        data = {
+            'video': video,
+            'user': request.user,
+            'type': type,
+            'start': time(**seconds_to_hms(request.data['start'])),
+            'end': time(**seconds_to_hms(request.data['end'])),
+            'labeled': True,
+        }
+        try:
+            Highlight.objects.create(**data)
+        except Exception as e:
+            return Response(data=repr(e), status=status.HTTP_400_BAD_REQUEST)
+        return Response(status=status.HTTP_200_OK)
 
 
 class HighlightDetail(generics.RetrieveUpdateDestroyAPIView):
