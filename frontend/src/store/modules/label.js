eedef3a42f3501632e086da23099d2dd718ab3b9
Rodrigo Mendez <rodmendezp@gmail.com>
2018-10-15 11:16:51 -0300

    set game, streamer and user

--- frontend/src/store/modules/label.js
+++ frontend/src/store/modules/label.js
@@ -1,14 +1,29 @@
 const localState = {
     keepLabeling: true,
+    game: null,
+    streamer: null,
+    user: null,
 };
 
 const getters = {
     getKeepLabeling: s => s.keepLabeling,
+    getGame: s => s.game,
+    getStreamer: s => s.streamer,
+    getUser: s => s.user,
 };
 
 const mutations = {
     setKeepLabeling(s, keepLabeling) {
         s.keepLabeling = keepLabeling;
+    },
+    setGame(s, game) {
+        s.game = game;
+    },
+    setStreamer(s, streamer) {
+        s.streamer = streamer;
+    },
+    setUser(s, user) {
+        s.user = user;
     },
 };
 
