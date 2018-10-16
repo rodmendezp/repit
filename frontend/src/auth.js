738c6676c9bddd7451a9cd38d85b618aae49a841
Rodrigo Mendez <rodmendezp@gmail.com>
2018-10-16 10:05:17 -0300

    added store to auth

--- frontend/src/auth.js
+++ frontend/src/auth.js
@@ -1,15 +1,19 @@
 /* eslint-disable no-new */
+import Vue from 'vue';
+import Vuex from 'vuex';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
-import Vue from 'vue';
 import Auth from './AuthApp';
 import AuthRouter from './router/auth';
+import store from './store';
 
+Vue.use(Vuex);
 Vue.config.productionTip = false;
 
 new Vue({
     el: '#auth',
     router: AuthRouter,
+    store,
     template: '<Auth/>',
     components: { Auth },
 });
