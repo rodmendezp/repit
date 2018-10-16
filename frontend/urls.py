42644d509dbdbaf534c2bf67cc7972499bcd0f15
Rodrigo Mendez <rodmendezp@gmail.com>
2018-10-15 23:57:35 -0300

    added user activation request

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from frontend import views
-from backend.requests import is_user
+from backend.requests import is_user, user_activation
 
 
 urlpatterns = [
@@ -9,5 +9,7 @@ urlpatterns = [
     url(r'^accounts/login/$', views.EmailLoginView.as_view()),
     url(r'^accounts/register/', views.UserCreate.as_view()),
     url(r'^accounts/is_user/', is_user),
+    url(r'^accounts/activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        user_activation, name='user_activation'),
     url(r'^app/$', views.app),
 ]
