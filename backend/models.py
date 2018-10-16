ca355fc26ae6926b96b76dcfd5839caaae2ced75
Rodrigo Mendez <rodmendezp@gmail.com>
2018-10-15 23:56:52 -0300

    added send activation and remove unnecesary uuid confirmation

--- backend/models.py
+++ backend/models.py
@@ -1,11 +1,21 @@
 import six
+import sendgrid
+from sendgrid.helpers.mail.email import Email
+from sendgrid.helpers.mail.content import Content
+from sendgrid.helpers.mail.mail import Mail
+from .tokens import account_activation_token
 from django.db import models
 from django.utils import timezone
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+from django.template.loader import render_to_string
+from django.contrib.sites.models import Site
 from django.contrib.auth.models import Group, PermissionsMixin, UserManager as DjangoUserManager
 from django.utils.translation import ugettext_lazy
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.validators import UnicodeUsernameValidator, ASCIIUsernameValidator
 from django.contrib.auth.base_user import AbstractBaseUser
+from Repit.settings import SENDGRID_API_KEY, ADMIN_EMAIL
 
 
 class UserManager(DjangoUserManager):
@@ -35,7 +45,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(ugettext_lazy('email address'), unique=True, blank=False)
     is_staff = models.BooleanField(ugettext_lazy('staff status'), default=False)
     # TODO: Change is_active default to false when enable email confirmation
-    is_active = models.BooleanField(ugettext_lazy('active'), default=True)
+    is_active = models.BooleanField(ugettext_lazy('active'), default=False)
     date_joined = models.DateTimeField(ugettext_lazy('date joined'), default=timezone.now)
 
     objects = UserManager()
@@ -55,18 +65,34 @@ class User(AbstractBaseUser, PermissionsMixin):
     def get_short_name(self):
         return self.first_name
 
-    # def email_user(self, subject, message, from_email=None, **kwargs):
-    #     """
-    #     :param subject:
-    #     :param message:
-    #     :param from_email:
-    #     :param kwargs:
-    #     :return:
-    #     """
-    #     pass
-    #
-    # def send_activation(self, template=None, domain=None):
-    #     pass
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        to_email = kwargs['to_email'] if 'to_email' in kwargs else self.email
+        sg = sendgrid.SendGridAPIClient(apikey=SENDGRID_API_KEY)
+        from_email = Email(from_email)
+        to_email = Email(to_email)
+        content_type = kwargs['content_type'] if 'content_type' in kwargs else 'text/plain'
+        content = Content(  content_type, message)
+        email = Mail(from_email, subject, to_email, content)
+        response = sg.client.mail.send.post(request_body=email.get())
+        return response
+
+    def send_activation(self, template=None, domain=None):
+        self.email_template = template or 'emails/registration/activate.html'
+        domain = domain or Site.objects.get_current().domain
+        self.email_params = {
+            'user': self,
+            'domain': domain,
+            'uid': urlsafe_base64_encode(force_bytes(self.pk)).decode("utf-8"),
+            'token': account_activation_token.make_token(self),
+        }
+        message = render_to_string(self.email_template, self.email_params)
+        self.email_response = self.email_user(
+            'Welcome To Repit',
+            message,
+            ADMIN_EMAIL,
+            content_type='text/html'
+        )
+        return self.email_response, self.email_template, self.email_params
 
     class Meta:
         verbose_name = ugettext_lazy('user')
