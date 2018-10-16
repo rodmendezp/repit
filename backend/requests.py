7b4483484bf7930a98d105935d6c3f10eec3bae1
Rodrigo Mendez <rodmendezp@gmail.com>
2018-10-15 23:57:17 -0300

    added user activation request

--- backend/requests.py
+++ backend/requests.py
@@ -1,7 +1,25 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseNotFound
+from django.shortcuts import redirect
+from django.utils.http import urlsafe_base64_decode
 from django.contrib.auth import get_user_model
+from django.utils.encoding import force_text
 from django.core.validators import validate_email
 from django.core.exceptions import ValidationError
+from backend.tokens import account_activation_token
+
+
+def user_activation(request, uidb64, token):
+    if request.method == 'GET':
+        try:
+            uid = force_text(urlsafe_base64_decode(uidb64))
+            user_model = get_user_model()
+            user = user_model.objects.get(pk=uid)
+        except Exception as e:
+            return HttpResponseNotFound
+        if user and account_activation_token.check_token(user, token):
+            user.is_active = True
+            user.save()
+    return redirect('/')
 
 
 def is_user(request):
@@ -12,7 +30,7 @@ def is_user(request):
         except ValidationError:
             return JsonResponse({'message': 'Invalid email.'}, status=400)
         try:
-            user = user_model.objects.get(email=request.GET.get('email'))
+            user_model.objects.get(email=request.GET.get('email'))
             return JsonResponse({}, status=200)
         except user_model.DoesNotExist:
             return JsonResponse({}, status=404)
