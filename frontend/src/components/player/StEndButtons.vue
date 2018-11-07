3081e1e4b7fb64c503d3ebf999d0cd0daecee72c
Rodrigo Mendez <rodmendezp@gmail.com>
2018-11-07 10:30:18 -0300

    component to modify start or end of video with buttons

---
+++ frontend/src/components/player/StEndButtons.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <button @click=""></button>
+        <button @mousedown=""></button>
+        <button @click=""></button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'StEndButtons',
+        props: {
+            time: { type: Number },
+            isStart: { },
+        },
+        mounted() {
+            console.log(this.time);
+            console.log(this.isStart);
+        },
+        watch: {
+            time(oldVal, newVal) {
+                console.log('oldVal = ', oldVal);
+                console.log('newVal = ', newVal);
+            },
+        },
+    };
+</script>
+
+<style scoped>
+
+</style>
