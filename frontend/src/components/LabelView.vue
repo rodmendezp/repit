ba0071d41dd70ef469adf2059accba3488c80bc5
Rodrigo Mendez <rodmendezp@gmail.com>
2018-11-28 21:20:11 -0300

    removed description, use type component, change center css using flex and left keep labeling for later

--- frontend/src/components/LabelView.vue
+++ frontend/src/components/LabelView.vue
@@ -1,44 +1,22 @@
 <template>
-    <div class="row full-height align-items-center">
-        <div class="col"></div>
-        <div class="col" v-if="status === 'idle'">
+    <!--<td class="left-td"><label for="id_keep_labeling">Keep Labeling</label></td>-->
+    <!--<td><input type="checkbox" :checked="keepLabeling" @input="keepLabelingCheckboxInput" id="id_keep_labeling"/></td>-->
+    <div class="main-container full-height">
+        <div class="content-container full-height" v-if="status === 'idle'">
             <twitch-player v-if="highlight"
-                :videoId="highlight.video_id"
-                :stTime="highlight.st_time"
-                :endTime="highlight.end_time"
+                           :videoId="highlight.video_id"
+                           :stTime="highlight.st_time"
+                           :endTime="highlight.end_time"
             ></twitch-player>
-            <div class="label-form">
-                <table class="table-form">
-                    <tr class="height-tr">
-                        <td class="left-td"><label for="id_description">Description</label></td>
-                        <td class="left-td"><input v-model="label.description" id="id_description"/></td>
-                    </tr>
-                    <tr class="height-tr">
-                        <td class="left-td"><label>Type</label></td>
-                        <td>
-                            <b-dropdown variant="primary" class="btn-primary type-dropdown" :text="label.type">
-                                <b-dropdown-item class="type-dropdown-item" v-for="labelType in types" :key="labelType"
-                                                 :value="labelType" @click="setType(labelType)">
-                                    {{ labelType }}
-                                </b-dropdown-item>
-                            </b-dropdown>
-                        </td>
-                    </tr>
-                    <tr class="height-tr">
-                        <td class="left-td"><label for="id_keep_labeling">Keep Labeling</label></td>
-                        <td><input type="checkbox" :checked="keepLabeling" @input="keepLabelingCheckboxInput" id="id_keep_labeling"/></td>
-                    </tr>
-                </table>
-                <button class="btn btn-primary" @click="submitLabel">Submit</button>
-            </div>
+            <label-type @type-setted="typeSetted = true" class="label-type-container"></label-type>
+            <button :disabled="!typeSetted" class="btn btn-primary" @click="submitLabel">Submit</button>
         </div>
-        <div style="text-align: center" class="col" v-else-if="status === 'processing'">
-            <div> LOADING </div>
+        <div v-else-if="status === 'processing'">
+            <h1>LOADING</h1>
         </div>
-        <div style="text-align: center" class="col" v-else-if="status === 'exception'">
-            <div> {{ exception }} </div>
+        <div v-else-if="status === 'exception'">
+            <h1>{{ exception }}</h1>
         </div>
-        <div class="col"></div>
     </div>
 </template>
 
@@ -47,10 +25,12 @@
     import bDropdownItem from 'bootstrap-vue/es/components/dropdown/dropdown-item';
     import bDropdown from 'bootstrap-vue/es/components/dropdown/dropdown';
     import TwitchPlayer from './player/TwitchPlayer';
+    import LabelType from './label/LabelType';
 
     export default {
         name: 'LabelView',
         components: {
+            LabelType,
             TwitchPlayer,
             bDropdown,
             bDropdownItem,
@@ -63,6 +43,7 @@
                 },
                 labelTypes: [],
                 exception: '',
+                typeSetted: false,
             };
         },
         methods: {
@@ -83,7 +64,6 @@
                 setDeliveryTag: 'filler/setDeliveryTag',
             }),
             submitLabel() {
-                if (this.label.type === 'Choose Type') return;
                 this.setHighlightStart(this.videoStartTime);
                 this.setHighlightEnd(this.videoEndTime);
                 this.postHighlight(this.highlight);
@@ -148,33 +128,26 @@
 </script>
 
 <style scoped lang="sass">
-    .label-form
-        margin-top: 90px
-        text-align: center
+    .main-container
+        display: flex
+        flex-direction: column
+        justify-content: center
+        align-items: center
 
-    .table-form
-        margin: 0 auto 20px
+    .content-container
+        display: flex
+        flex-direction: column
+        justify-content: center
+        align-items: center
 
-    .left-td
-        text-align: left
+    .label-type-container
+        margin-top: 15px
 
-    .height-tr
-        height: 40px
+    /*@media only screen and (min-width: 1024px)*/
+        /*.content-container*/
+            /*justify-content: center*/
 
-    label
-        color: white
-        margin-bottom: 0
-
-    .type-dropdown
-        background-color: transparent
-
-    .form-block
-        margin-top: 5px
-        margin-bottom: 5px
-
-    .form-block label
-        display: inline-block
-        width: 100px
-        text-align: right
-        color: white
+    /*@media only screen and (orientation: landscape) and (max-width: 1023px)*/
+        /*.content-container*/
+            /*width: 100%*/
 </style>
