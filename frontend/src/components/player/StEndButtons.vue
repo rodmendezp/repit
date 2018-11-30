e7d4287060e84aec0da62ad3323469263c56608d
Rodrigo Mendez <rodmendezp@gmail.com>
2018-11-30 15:06:48 -0300

    added functionality to go to current time by holding

--- frontend/src/components/player/StEndButtons.vue
+++ frontend/src/components/player/StEndButtons.vue
@@ -3,10 +3,14 @@
         <button :disabled="isStartCursorLeft" class="btn-fa-style" @click="setExtraTimeZero(isStart)" v-if="isStart">
             <i class="fa fa-backward"></i>
         </button>
-        <button :disabled="(!isStart && isStartEqualEnd) || (isStart && isStartCursorLeft)"
-                class="btn-fa-style" @click="modifyTime(isStart, -1)"><i class="fa fa-minus"></i></button>
-        <button :disabled="(isStart && isStartEqualEnd) || (!isStart && isEndCursorRight)"
-                class="btn-fa-style" @click="modifyTime(isStart, 1)"><i class="fa fa-plus"></i></button>
+        <button :disabled="isMinusDisabled" class="btn-fa-style"
+                @click="modifyTime(isStart, -1)" @mousedown="mouseDown(true)" @mouseup="mouseUp(true)">
+            <i class="fa fa-minus"></i>
+        </button>
+        <button :disabled="isPlusDisabled" class="btn-fa-style"
+                @click="modifyTime(isStart, 1)" @mousedown="mouseDown(false)" @mouseup="mouseUp(false)">
+            <i class="fa fa-plus"></i>
+        </button>
         <button :disabled="isEndCursorRight" class="btn-fa-style" @click="setExtraTimeZero(isStart)" v-if="!isStart">
             <i class="fa fa-forward"></i>
         </button>
@@ -21,6 +25,11 @@
         props: {
             time: { type: Number },
             isStart: { type: Boolean },
+        },
+        data() {
+            return {
+                isMouseDown: [false, false],
+            };
         },
         methods: {
             ...mapMutations({
@@ -58,14 +67,37 @@
                     modifyExtraFunc(extraTime + ((isStart ? 1 : -1) * deltaSeconds));
                 }
             },
+            mouseDown(isMinus) {
+                if (this.isStart && isMinus) return;
+                else if (!this.isStart && !isMinus) return;
+                this.isMouseDown[isMinus ? 0 : 1] = true;
+                setTimeout(() => this.setCursorCurrentTime(!isMinus), 500);
+            },
+            mouseUp(isMinus) {
+                if (this.isMouseDown[isMinus ? 0 : 1]) this.isMouseDown[isMinus ? 0 : 1] = false;
+            },
+            setCursorCurrentTime(isStart) {
+                if (this.isMouseDown[!isStart ? 0 : 1]) {
+                    const time = isStart ? this.videoStartTime : this.videoEndTime;
+                    this.modifyTime(isStart, this.videoTime - time);
+                    if (this.isStart) setTimeout(() => this.setCursorCurrentTime(isStart), 500);
+                }
+            },
         },
         computed: {
             ...mapGetters({
+                videoTime: 'player/getVideoTime',
                 videoStartTime: 'player/getVideoStartTime',
                 videoEndTime: 'player/getVideoEndTime',
                 extraTimeLeft: 'player/getExtraTimeLeft',
                 extraTimeRight: 'player/getExtraTimeRight',
             }),
+            isMinusDisabled() {
+                return (!this.isStart && this.isStartEqualEnd) || (this.isStart && this.isStartCursorLeft);
+            },
+            isPlusDisabled() {
+                return (this.isStart && this.isStartEqualEnd) || (!this.isStart && this.isEndCursorRight);
+            },
             isStartCursorLeft() {
                 return this.extraTimeLeft === 0;
             },
